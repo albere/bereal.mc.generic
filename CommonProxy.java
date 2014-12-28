48d9478331d2863262505776124d4bdd5da126b4
Bereal <albere@albere.net>
2014-12-28 09:38:33 +0000

    Adding mc boilerplate

---
+++ CommonProxy.java
@@ -0,0 +1,9 @@
+package bereal.mc.generic;
+
+public class CommonProxy {
+
+        // Client stuff
+        public void registerRenderers() {
+                // Nothing here as the server doesn't render graphics or entities!
+        }
+}
---
+++ client/ClientProxy.java
@@ -0,0 +1,13 @@
+package bereal.mc.generic.client;
+
+import net.minecraftforge.client.MinecraftForgeClient;
+import bereal.mc.generic.CommonProxy;
+
+public class ClientProxy extends CommonProxy {
+       
+        @Override
+        public void registerRenderers() {
+                // This is for rendering entities and so forth later on
+        }
+       
+}
