447424f6d12e4d4145de85440caa706321bc16c3
Bereal <albere@albere.net>
2014-12-28 12:27:56 +0000

    adding genericblock

---
+++ GenericBlock.java
@@ -0,0 +1,15 @@
+package bereal.mc.generic;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class GenericBlock extends Block
+{
+
+        public GenericBlock (Material material)
+        {
+                super(material);
+        	    setHarvestLevel("shovel", 0);
+        }
+}
+
